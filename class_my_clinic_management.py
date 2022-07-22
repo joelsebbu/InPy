1705d15c98c0237f2c95c4f85e5fa905b602338b
joelsebbu <joel18sebastian@gmail.com>
2022-07-22 11:49:45 +0530

    listAll and search fixed, display now private

--- class_my_clinic_management.py
+++ class_my_clinic_management.py
@@ -11,13 +11,23 @@ class Patient:
             self.blood=blood
 
 class Clinic:
+
+    class Patient:
+        def __init__(self, id, name, age, dob, gender, blood):
+            self.id=id
+            self.name=name
+            self.age=age
+            self.dob=dob
+            self.gender=gender
+            self.blood=blood
+
     def __init__(self,name,address):
         self.name=name
         self.address=address
         self.patients={}
         self.patientCount=0
 
-    def display(self,id):
+    def __display(self,id):
         print(self.patients[id].id)
         print(self.patients[id].name)
         print(self.patients[id].age)
@@ -25,20 +35,22 @@ class Clinic:
         print(self.patients[id].gender)
         print(self.patients[id].blood)
 
-    def admit(self,name,age,dob,gender,blood):
+    def register(self,name,age,dob,gender,blood):
         id=self.patientCount
         self.patients[id]=(Patient(id,name,age,dob,gender,blood))
         id+=1
         self.patientCount=id
         
     def search(self,id):
-        self.display(id)
+        self.__display(id)
+
     def delete(self,name):
         for patient in self.patients:
             if patient.name==name:
                 self.patients.remove(patient)
                 return
         return None
+
     def update(self,name,age):
         for patient in self.patients:
             if patient.name==name:
@@ -46,9 +58,8 @@ class Clinic:
                 return
         return None
     def listAll(self):
-        #print(self.patients[0].name,self.patients[0].age)
-        for patient in self.patients:
-             self.display(patient) 
+        for id in self.patients:
+             self.__display(id) 
 
 clinic = Clinic("clinic","address")
 clinic.admit("jane",20,"12/12/12","F","A+")
