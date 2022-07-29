c7f5c5a87cb2f747a23dcd57f978fa12e4b4c056
joelsebbu <joel18sebastian@gmail.com>
2022-07-29 16:07:25 +0530

    train book works

--- train_management.py
+++ train_management.py
@@ -17,10 +17,10 @@ def connectDB(func):
             return None
         else:
             cursor = conn.cursor()
-            value = func(cursor, *args)
+            rt = func(cursor, *args)
             conn.commit()
             conn.close()
-            return value
+            return rt
     return innerWrapper
 
 class Booking:
@@ -36,16 +36,45 @@ class Booking:
             select count(*) from {trainId}
             where waitList =0;'''
         )
-        count=cursor.fetchall()
-        return count[0][0]
+        count =cursor.fetchall()
+        rt =(trainId,'unavailable')
+        if count[0][0] <=5:
+            rt =(trainId,'available')
+        else:
+            print(trainId)
+            cursor.execute(f"select _index from Trains where trainId = '{trainId}';")
+            index=cursor.fetchall()
+            nextIndex=index[0][0]+1
+            if nextIndex <= 3: #static #next train shift
+                cursor.execute(f"select trainId from Trains where _index = '{nextIndex}'")
+                self.check(cursor.fetchall()[0][0])
+            else: # to wait list
+                pass
+        print(rt)
+        return rt
+
+    @connectDB
+    def bookAt(cursor,self,trainId,aadhar,name,fromId,toId):
+        #cursor.execute(f"insert into Bookings values({aadhar},'{name}','{trainId}');")
+        cursor.execute(f"select stId from Stations where name = '{fromId}'")
+        fromId =cursor.fetchall()[0][0]
+        cursor.execute(f"select stId from Stations where name = '{toId}'")
+        toId =cursor.fetchall()[0][0]
+        cursor.execute(f"insert into {trainId} values({aadhar},{fromId},{toId},0);")
 
     def book(self,aadhar,name,fromId,toId):
         trainId = fromId+'_'+toId
-        print(trainId)
         rt=self.check(trainId)
-        
+        #print(rt)
+        if rt[1] == 'Unavailable':
+            print("No Seats Available")
+        elif rt[1] == 'available':
+            self.bookAt(rt[0],aadhar,name,fromId,toId,)
+            print("Booked a seat on "+rt[0]+". Use your aadhar as ticket")
+        elif rt[1] == 'waitList':
+            print("You are in the wait list for the train: "+rt[0])
 
 
 south =Booking()
-south.book(5454,'Joel','TVM','ERN')
+south.book(5456,'Joel','TVM','ERN')
 
